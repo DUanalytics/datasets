62c9d3cf81a0323b015f7a58dd2ed1b2543194d2
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-12-08 08:19:51 +0530

    a

--- rdata/rdataTocsv.R
+++ rdata/rdataTocsv.R
@@ -1,8 +1,6 @@
 
 
-url='https://github.com/DUanalytics/datasets/blob/master/rdata/titanic.raw.rdata'
-load(url)
-#download and then load
+
 
 load("E:/analytics/projects/datasets/rdata/titanic.raw.rdata")
 
@@ -15,3 +13,39 @@ url ='https://raw.githubusercontent.com/DUanalytics/datasets/master/csv/titanic.
 
 titanicdata = read.csv(url)
 head(titanicdata)
+
+saveRDS(titanicdata, file='rdata/titanic.RDS')
+
+#
+githubURL <- "https://github.com/thefactmachine/hex-binning-gis-data/raw/master/popDensity.RData"
+load(url(githubURL))
+head(df)
+dim(df)
+
+#-----
+"https://github.com/DUanalytics/datasets/blob/master/rdata/titanic.RDS?raw=true"
+"https://github.com/DUanalytics/datasets/blob/master/rdata/raw/master/titanic.RDS"
+
+"https://github.com/DUanalytics/datasets/blob/master/rdata/titanic.RDS"
+
+
+githubURL <- "https://github.com/DUanalytics/datasets/blob/master/rdata/titanic.RDS?raw=true"
+df=readRDS(url(githubURL))
+head(df)
+
+#Rdata
+data()
+# Saving on object in RData format
+save(mtcars, file = "rdata/data.RData")
+# Save multiple objects
+save(iris, USArrests, file = "rdata/data.RData")
+# To load the data again
+load("data.RData")
+
+#now load from url
+
+
+
+url2="https://github.com/DUanalytics/datasets/blob/master/rdata/titanic.raw.Rdata"
+load(url(url2))
+#download and then load
