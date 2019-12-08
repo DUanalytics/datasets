e951da1e914c5dece5a0fa476e1b2d73f2a5d47f
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-12-08 07:57:58 +0530

    a

--- rdata/rdataTocsv.R
+++ rdata/rdataTocsv.R
@@ -1,4 +1,17 @@
 
+
+url='https://github.com/DUanalytics/datasets/blob/master/rdata/titanic.raw.rdata'
+load(url)
+#download and then load
+
+load("E:/analytics/projects/datasets/rdata/titanic.raw.rdata")
+
 titanic.raw
 
+
 write.csv(titanic.raw, 'csv/titanic.csv', row.names = F)
+
+url ='https://raw.githubusercontent.com/DUanalytics/datasets/master/csv/titanic.csv'
+
+titanicdata = read.csv(url)
+head(titanicdata)
