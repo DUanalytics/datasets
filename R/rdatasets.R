19f196ce0c747f11319f5a56ae24e7d3e5923a6a
Dhiraj Upadhyaya <dupadhyaya@users.noreply.github.com>
2020-08-13 20:34:11 +0530

    a

--- R/rdatasets.R
+++ R/rdatasets.R
@@ -1,3 +1,12 @@
 #R Data Sets
 
 data()
+
+AirPassengers
+write.csv(AirPassengers,'R/airpassengers.csv')
+
+BJsales
+write.csv(BJsales,'R/bjsales.csv')
+
+BOD #                    Biochemical Oxygen Demand
+write.csv(BOD,'R/bod.csv')
